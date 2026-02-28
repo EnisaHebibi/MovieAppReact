f9a341d333cbdfa54999cb3d77bd8c47b0484fe8
Enisa Hebibi <isahebibi@gmail.com>
2026-02-28 06:28:47 +0100

    set up Appwrite to use its database functionalities

--- MovieApp/src/App.jsx
+++ MovieApp/src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import SpinnerEmpty from "./components/SpinnerEmpty";
 import MovieCard from "./components/MovieCard";
 import { useDebounce } from "react-use";
+import { updateSearchCount } from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_URL;
@@ -39,7 +40,6 @@ const App = () => {
       }
 
       const data = await response.json();
-      console.log(data);
 
       if (data.Response === "False") {
         setErrorMessage(data.Error || "Failed to fetch movies");
@@ -48,6 +48,9 @@ const App = () => {
       }
 
       setMovieList(data.results || []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
     } catch (error) {
       console.error(`Error fetching movies:${error}`);
       setErrorMessage("Error fetching movies. Please try again later.");
